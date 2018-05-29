4f7358ecf4beb9539f49f646a48358a03172a01a
Crystor Zhao <crystor9@uw.edu>
2018-05-28 17:39:29 -0700

    table

--- scripts/build_table.R
+++
@@ -1,7 +0,0 @@
-library(plotly)
-library(stringr)
-library(ggplot2)
-library(dplyr)
-
-source("scripts/top_tracks.R")
-combined_data_frame$track_name <- paste0("<a href=\"", combined_data_frame$link, "\"", combined_data_frame$track_name, "</a>")
